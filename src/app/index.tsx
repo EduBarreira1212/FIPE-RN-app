c12976b45e0b1228ea65f9623aa10df26f688206
Eduardo Barreira <edubarreira1212@gmail.com>
2025-10-07 20:57:33 -0300

    feat: use SafeAreaView component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Page() {
   return (
-    <View>
+    <SafeAreaView>
       <View className="bg-black h-full items-center justify-center">
         <Text className="text-red-500">FIPE APP</Text>
+        <Link href={"/sign-in"} className="text-white">
+          Login
+        </Link>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
