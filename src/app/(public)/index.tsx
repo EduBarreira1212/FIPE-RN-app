fd2408fa859be0e617a33877c29a1d7e31d77d98
Eduardo Barreira <edubarreira1212@gmail.com>
2025-10-08 19:09:40 -0300

    feat: create app initial page

--- src/app/(public)/index.tsx
+++ src/app/(public)/index.tsx
@@ -1,19 +1,33 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, ImageBackground, Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Page() {
   return (
-    <SafeAreaView>
-      <View className="bg-black h-full items-center justify-center">
-        <Text className="text-red-500">FIPE APP</Text>
-        <Link href={"/sign-in"} className="text-white">
-          Login
-        </Link>
-        <Link href={"/sign-up"} className="text-white">
-          Create account
-        </Link>
-      </View>
-    </SafeAreaView>
+    <ImageBackground source={require("../../assets/onboarding-bg/app-bg.png")}>
+      <SafeAreaView>
+        <View className="h-full items-center justify-between">
+          <Image
+            source={require("../../assets/logo.png")}
+            className="w-[150px] h-[150px] "
+          />
+
+          <View className="w-full items-center mb-5">
+            <Link href={"/sign-up"} className="text-white" asChild>
+              <Pressable className="bg-green-400 w-3/4 items-center rounded-lg p-3 my-2 on active:bg-green-500">
+                <Text className="text-lg">Criar conta</Text>
+              </Pressable>
+            </Link>
+
+            <View className="flex-row">
+              <Text className="text-lg text-white">Já tem uma conta? </Text>
+              <Link href={"/sign-in"} className="text-white">
+                <Text className="text-lg text-green-400">Acesse agora!</Text>
+              </Link>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
