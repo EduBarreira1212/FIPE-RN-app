118747a64537b3e72c4ee25dc76d0537c0f19818
Eduardo Barreira <edubarreira1212@gmail.com>
2025-10-08 17:46:31 -0300

    refactor: implement useAuth hook to handle auth

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,21 +1,31 @@
 import { Stack } from "expo-router";
 import "../styles/global.css";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { AuthProvider } from "../contexts/AuthContext";
+import { useAuth } from "../hooks/useAuth";
 
-export default function RootLayout() {
-  const isLoggedIn = false;
-
+export default function Layout() {
   return (
     <SafeAreaProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Protected guard={isLoggedIn}>
-          <Stack.Screen name="(private)" />
-        </Stack.Protected>
-
-        <Stack.Protected guard={!isLoggedIn}>
-          <Stack.Screen name="(public)" />
-        </Stack.Protected>
-      </Stack>
+      <AuthProvider>
+        <RootLayout />
+      </AuthProvider>
     </SafeAreaProvider>
+  );
+}
+
+function RootLayout() {
+  const { isLoggedIn, isLoading } = useAuth();
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Protected guard={isLoggedIn}>
+        <Stack.Screen name="(private)" />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!isLoggedIn}>
+        <Stack.Screen name="(public)" />
+      </Stack.Protected>
+    </Stack>
   );
 }
