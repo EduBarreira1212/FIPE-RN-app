ab93efc94013a43f6a397ba97417a86f6acffa2c
Eduardo Barreira <edubarreira1212@gmail.com>
2025-10-08 17:22:29 -0300

    feat: create protection for routes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,9 +3,19 @@ import "../styles/global.css";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
+  const isLoggedIn = false;
+
   return (
     <SafeAreaProvider>
-      <Stack screenOptions={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Protected guard={isLoggedIn}>
+          <Stack.Screen name="(private)" />
+        </Stack.Protected>
+
+        <Stack.Protected guard={!isLoggedIn}>
+          <Stack.Screen name="(public)" />
+        </Stack.Protected>
+      </Stack>
     </SafeAreaProvider>
   );
 }
