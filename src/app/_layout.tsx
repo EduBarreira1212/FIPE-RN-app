080193d4a2494705298cebe64e55dfb0ef869362
Eduardo Barreira <edubarreira1212@gmail.com>
2025-10-09 22:56:58 -0300

    feat: ensure assets are pre loaded before hide splash screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,12 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { Asset } from "expo-asset";
 import "../styles/global.css";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AuthProvider } from "../contexts/AuthContext";
 import { useAuth } from "../hooks/useAuth";
+import { useCallback, useEffect, useState } from "react";
+import { View } from "react-native";
 
 export default function Layout() {
   return (
@@ -16,16 +20,43 @@ export default function Layout() {
 
 function RootLayout() {
   const { isLoggedIn, isLoading } = useAuth();
+  const [assetsReady, setAssetsReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      await SplashScreen.preventAutoHideAsync();
+      try {
+        await Asset.loadAsync([
+          require("../assets/onboarding-bg/app-bg.png"),
+          require("../assets/logo.png"),
+        ]);
+      } finally {
+        setAssetsReady(true);
+      }
+    })();
+  }, []);
+
+  const appReady = assetsReady && !isLoading;
+
+  const onLayout = useCallback(async () => {
+    if (appReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appReady]);
 
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Protected guard={isLoggedIn}>
-        <Stack.Screen name="(private)" />
-      </Stack.Protected>
+    <View style={{ flex: 1 }} onLayout={onLayout}>
+      {appReady ? (
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Protected guard={isLoggedIn}>
+            <Stack.Screen name="(private)" />
+          </Stack.Protected>
 
-      <Stack.Protected guard={!isLoggedIn}>
-        <Stack.Screen name="(public)" />
-      </Stack.Protected>
-    </Stack>
+          <Stack.Protected guard={!isLoggedIn}>
+            <Stack.Screen name="(public)" />
+          </Stack.Protected>
+        </Stack>
+      ) : null}
+    </View>
   );
 }
