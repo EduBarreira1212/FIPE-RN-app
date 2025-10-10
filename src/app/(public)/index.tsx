dcf9672dfb7210c682155fa4a54bb2872eb41d5b
Eduardo Barreira <edubarreira1212@gmail.com>
2025-10-09 22:27:06 -0300

    feat: add overlay and explanation text

--- src/app/(public)/index.tsx
+++ src/app/(public)/index.tsx
@@ -5,6 +5,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Page() {
   return (
     <ImageBackground source={require("../../assets/onboarding-bg/app-bg.png")}>
+      <View className="absolute inset-0 bg-black/40" />
       <SafeAreaView>
         <View className="h-full items-center justify-between">
           <Image
@@ -12,17 +13,25 @@ export default function Page() {
             className="w-[150px] h-[150px] "
           />
 
-          <View className="w-full items-center mb-5">
+          <View className="w-full items-center mb-5 gap-3">
+            <View className="w-[90%]">
+              <Text className="text-white text-2xl font-semibold text-center leading-snug">
+                Obtenha informações do veículo e valor da tabela FIPE apenas com
+                a placa
+              </Text>
+            </View>
             <Link href={"/sign-up"} className="text-white" asChild>
-              <Pressable className="bg-green-400 w-3/4 items-center rounded-lg p-3 my-2 on active:bg-green-500">
-                <Text className="text-lg">Criar conta</Text>
+              <Pressable className="bg-green-400 w-3/4 items-center rounded-lg p-3 my-2 active:bg-green-500">
+                <Text className="text-lg font-semibold">Criar conta</Text>
               </Pressable>
             </Link>
 
             <View className="flex-row">
               <Text className="text-lg text-white">Já tem uma conta? </Text>
               <Link href={"/sign-in"} className="text-white">
-                <Text className="text-lg text-green-400">Acesse agora!</Text>
+                <Text className="text-lg text-green-400 underline">
+                  Acesse agora!
+                </Text>
               </Link>
             </View>
           </View>
