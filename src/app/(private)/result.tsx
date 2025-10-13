0093cf7377399ea18d8af64e39c4ff5f743ea4fe
Eduardo Barreira <edubarreira1212@gmail.com>
2025-10-13 18:17:40 -0300

    refactor: use FooterBtn component in result page

--- src/app/(private)/result.tsx
+++ src/app/(private)/result.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
+import FooterBtn from "../../components/FooterBtn";
 
 const ResultPage = () => {
   const router = useRouter();
@@ -18,20 +19,18 @@ const ResultPage = () => {
         </View>
         <View className="flex-1 border-t border-b border-gray-300 my-4 w-full items-center justify-center gap-5"></View>
         <View className="w-full items-center justify-around flex-row">
-          <Pressable
-            className="items-center gap-1"
+          <FooterBtn
+            active={false}
+            icon={"search-outline"}
+            text="Consultar"
             onPress={() => router.push("/")}
-          >
-            <Ionicons name="search-outline" size={35} />
-            <Text className="font-semibold">Consultar</Text>
-          </Pressable>
-          <Pressable
-            className="items-center gap-1"
+          />
+          <FooterBtn
+            active={false}
+            icon={"refresh-outline"}
+            text="Histórico"
             onPress={() => router.push("/history")}
-          >
-            <Ionicons name="refresh-outline" size={35} />
-            <Text className="font-semibold">Histórico</Text>
-          </Pressable>
+          />
         </View>
       </View>
     </SafeAreaView>
