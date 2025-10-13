ea66fadb74a3ba4e8acd81518629502c98e9e910
Eduardo Barreira <edubarreira1212@gmail.com>
2025-10-13 17:41:03 -0300

    feat: create FIPE request result page

---
+++ src/app/(private)/result.tsx
@@ -0,0 +1,41 @@
+import { router, useRouter } from "expo-router";
+import React from "react";
+import { Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+
+const ResultPage = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView>
+      <View className="h-full items-center justify-between relative">
+        <View className="flex-row w-full px-3 items-center justify-center relative">
+          <Pressable className="absolute left-3" onPress={() => router.back()}>
+            <Ionicons name="arrow-back" size={30} />
+          </Pressable>
+          <Text className="text-2xl text-center">Consulta FIPE</Text>
+        </View>
+        <View className="flex-1 border-t border-b border-gray-300 my-4 w-full items-center justify-center gap-5"></View>
+        <View className="w-full items-center justify-around flex-row">
+          <Pressable
+            className="items-center gap-1"
+            onPress={() => router.push("/")}
+          >
+            <Ionicons name="search-outline" size={35} />
+            <Text className="font-semibold">Consultar</Text>
+          </Pressable>
+          <Pressable
+            className="items-center gap-1"
+            onPress={() => router.push("/history")}
+          >
+            <Ionicons name="refresh-outline" size={35} />
+            <Text className="font-semibold">Histórico</Text>
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ResultPage;
