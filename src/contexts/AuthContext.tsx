ee1b4464a59bd62de2b5e466ac77da63fe7126b3
Eduardo Barreira <edubarreira1212@gmail.com>
2025-10-15 16:02:39 -0300

    feat: create auth logic

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
-import React, { createContext, useCallback } from "react";
+import React, { createContext, useCallback, useEffect, useState } from "react";
 import { httpClient } from "../services/httpClient";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type SignInParams = {
   email: string;
@@ -18,28 +19,67 @@ interface IAuthContextValue {
   isLoading: boolean;
   signIn(params: SignInParams): Promise<void>;
   signUp(params: SignUpParams): Promise<void>;
+  signOut(): Promise<void>;
 }
 
 export const AuthContext = createContext({} as IAuthContextValue);
 
+const TOKEN_STORAGE_KEY = "@fipe-app::token";
+
 export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [token, setToken] = useState<string | null>(null);
+  const [isLoadingToken, setIsLoadingToken] = useState(true);
+
+  useEffect(() => {
+    const load = async () => {
+      const data = await AsyncStorage.getItem(TOKEN_STORAGE_KEY);
+      setToken(data);
+      setIsLoadingToken(false);
+    };
+
+    load();
+  }, []);
+
+  useEffect(() => {
+    const run = async () => {
+      if (!token) {
+        return;
+      }
+
+      await AsyncStorage.setItem(TOKEN_STORAGE_KEY, token);
+    };
+
+    run();
+  }, [token]);
+
   const { mutateAsync: signIn } = useMutation({
     mutationFn: async (params: SignInParams) => {
       const { data } = await httpClient.post("/login", params);
-      console.log(data);
+      setToken(data);
     },
   });
 
   const { mutateAsync: signUp } = useMutation({
     mutationFn: async (params: SignUpParams) => {
       const { data } = await httpClient.post("/users", params);
-      console.log(data);
+      setToken(data);
     },
   });
 
+  const signOut = useCallback(async () => {
+    setToken(null);
+    await AsyncStorage.removeItem(TOKEN_STORAGE_KEY);
+  }, []);
+
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn: false, isLoading: false, signIn, signUp }}
+      value={{
+        isLoggedIn: !!token,
+        isLoading: isLoadingToken,
+        signIn,
+        signUp,
+        signOut,
+      }}
     >
       {children}
     </AuthContext.Provider>
