1437c0285a419ce88aeb64b3b4796dfd122ce74f
Eduardo Barreira <edubarreira1212@gmail.com>
2025-10-16 21:37:48 -0300

    feat: create axios interceptors to handle expired token

--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -1,5 +1,20 @@
 import axios from 'axios';
+import { TOKEN_STORAGE_KEY } from '../contexts/AuthContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { router } from 'expo-router';
 
 export const httpClient = axios.create({
     baseURL: 'https://adversarially-nondisciplinable-rayne.ngrok-free.dev/api'
-})
+});
+
+httpClient.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const status = err.response?.status;
+    if (status === 401) {
+      await AsyncStorage.removeItem(TOKEN_STORAGE_KEY);
+      router.push('/login');
+    }
+    return Promise.reject(err);
+  }
+);
