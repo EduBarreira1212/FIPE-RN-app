e3589b5ca2e4b6102142fe1e4533cd21fca8472f
Eduardo Barreira <edubarreira1212@gmail.com>
2025-10-20 17:29:51 -0300

    feat: change header text

--- src/app/(public)/sign-up.tsx
+++ src/app/(public)/sign-up.tsx
@@ -1,4 +1,3 @@
-import { Link } from "expo-router";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SignUpForm from "../../components/SignUpForm";
@@ -6,7 +5,7 @@ import SignUpForm from "../../components/SignUpForm";
 const SignUpPage = () => {
   return (
     <SafeAreaView className="flex-1 items-center">
-      <Text className="text-2xl">Crie sua conta</Text>
+      <Text className="text-2xl">Bem-vindo!</Text>
       <SignUpForm />
     </SafeAreaView>
   );
